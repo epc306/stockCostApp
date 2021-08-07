9be592e1e529c53ad9afb0743bcfafee65f6f968
water <mywater888@gmail.com>
2021-08-07 20:27:04 +0800

    Initial Commit

---
+++ stockCostApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stockCostApp
+//
+//  Created by water on 2021/8/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
